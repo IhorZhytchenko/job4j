e7bff5731ad8541133dca472319105025195befd
IhorZhytchenko <igor.zhytchenko@gmail.com>
2018-04-03 19:55:16 +0300

    1.3. JavDoc. Code convetion.

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,7 +1,17 @@
 package ru.job4j;
 
+/**
+ * Calculate.
+ *
+ * @author Ihor Zhytchenko (igor.zhytchenko@gmail.com)
+ * @version $1$
+ * @since 03.04.2018
+ */
 public class Calculate {
-   
+    /**
+     * Main.
+     * @param args - args.
+     */
 
     public static void main(String[] args) {
         System.out.println("Hello World");
