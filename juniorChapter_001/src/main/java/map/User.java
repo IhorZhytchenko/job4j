57d8a51b82c17c721156b806808b2ba5e06608d9
IhorZhytchenko <igor.zhytchenko@gmail.com>
2018-04-20 09:47:33 +0300

    User

---
+++ juniorChapter_001/src/main/java/map/User.java
@@ -0,0 +1,24 @@
+package map;
+
+import java.util.Calendar;
+
+public class User {
+    private String name;
+    private int children;
+    private Calendar birthday;
+
+    public User(String name, int children, Calendar birthday) {
+        this.name = name;
+        this.children = children;
+        this.birthday = birthday;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = 17;
+        result = 31 * result +  (name != null ? name.hashCode() : 0);
+        result = 31 * result + children;
+        result = 31 * result + (birthday != null ? birthday.hashCode() : 0);
+        return result;
+    }
+}
