1057c26815066602d5a9464cd2ec3a7fc37979eb
IhorZhytchenko <igor.zhytchenko@gmail.com>
2018-04-27 09:40:57 +0300

    Ping Pong

---
+++ juniorChapter_002/src/main/java/threads/RectangleMove.java
@@ -0,0 +1,50 @@
+package threads;
+
+import javafx.scene.shape.Rectangle;
+/**
+ * RectangleMove.
+ *
+ * @author Ihor Zhytchenko (igor.zhytchenko@gmail.com)
+ * @version $1$
+ * @since 27.04.2018
+ */
+public class RectangleMove implements Runnable {
+    private static final double SIZE_X = 300.0;
+    private final Rectangle rect;
+    private int step = 1;
+
+    public RectangleMove(Rectangle rect) {
+        this.rect = rect;
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+            this.move();
+            try {
+                Thread.sleep(50);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void move() {
+        border();
+        this.rect.setX(this.rect.getX() + this.step);
+    }
+
+    private void border() {
+        if (this.step == 1) {
+            if ((this.rect.getX() + this.rect.getWidth()) >= SIZE_X) {
+                this.step = -1;
+            }
+        }
+        if (this.step == -1) {
+            if (this.rect.getX() <= 0) {
+                this.step = 1;
+            }
+        }
+    }
+
+}
---
+++ juniorChapter_002/src/main/java/threads/package-info.java
@@ -0,0 +1,8 @@
+/**
+* Package for threads task.
+*
+* @author Ihor Zhytchenko (igor.zhytchenko@gmail.com)
+* @version $1$
+* @since 27.04.2018
+*/
+package threads;
